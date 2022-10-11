5bc109186e7d3769e0997b94e8e6e5a3fcba5c7a
Iswarya Devi Gomathi Sankar <ishu27.v@gmail.com>
2022-10-11 09:23:39 +0200

    Add a comment for print statement

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,7 +2,9 @@ package org.example;
 
 public class App {
     public static void main(String[] args) {
+        //statement for printing the data
         System.out.println("Hello world!");
     }
 }
+
 
