4d9630f29f65c3e185e991da6a0a0532b83a78a8
Iswarya Devi Gomathi Sankar <ishu27.v@gmail.com>
2022-10-11 11:32:34 +0200

    Print the statements

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,6 +4,40 @@ public class App {
     public static void main(String[] args) {
         //statement for printing the data
         System.out.println("Hello world!");
+
+        int age= 32;
+        System.out.println("Age: " + age);
+
+        double salary = 40000.00;
+        System.out.println("Salary:" +salary);
+
+        /*
+        boolean studentStatus =true;
+        boolean accountStatus =false;
+
+        char a = 'a';
+
+        float f =23.5f;
+
+         */
+
+        int number1=20;
+        int number2=10;
+        int result=number1 + number2;
+        System.out.println("Result:" +result);
+
+     int number = 2;
+     System.out.println(--number);
+
+     int num1 = 20;
+     int num2 = 20;
+        System.out.println(num1 == num2);
+
+
+
+
+
+
     }
 }
 
